8ba457bfbb92730c1bee839f717f14bd5bf214ea
OPersian <olena.persianova@gmail.com>
2016-11-10 00:09:02 +0200

    fix2

--- quadratic/views.py
+++ quadratic/views.py
@@ -14,15 +14,15 @@ def quadratic_results(request):
         form = QuadraticForm(request.GET)
 
         if form.is_valid():
-	        a = form.cleaned_data['a']
-	        b = form.cleaned_data['b']
-	        c = form.cleaned_data['c']
-	        # claculating discriminant
-	        d = b ** 2 - 4 * a * c
-	        if d < 0:
-		        result = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-	        elif d == 0:
-		        result = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (-b / 2 * a)
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            c = form.cleaned_data['c']
+            # claculating discriminant
+            d = b ** 2 - 4 * a * c
+            if d < 0:
+                result = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+            elif d == 0:
+                result = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (-b / 2 * a)
 
             else:
                 x1 = (-b + d ** (1/2.0)) / (2 * a)
