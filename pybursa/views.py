336940d913c4b5d15e557be510a407308981ff46
OPersian <olena.persianova@gmail.com>
2016-11-14 23:57:42 +0200

    added solution

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,17 +1,11 @@
-from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from courses.models import Course
+
 
 def index(request):
-    return render(request, "index.html")
+    courses = Course.objects.all()
+    return render(request, 'index.html', {'courses': courses})
+
 
 def contact(request):
-    return render(request, "contact.html")
-
-def student_list(request):
-    return render(request, "student_list.html")
-
-def student_detail(request):
-    return render(request, "student_detail.html")
-
-
-# Create your views here.
+    return render(request, 'contact.html')
