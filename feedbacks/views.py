e32d7f3b1fd55a0846e93e34327fdea2064a7120
OPersian <olena.persianova@gmail.com>
2016-11-15 19:09:57 +0200

    fix

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -4,12 +4,15 @@ from django.core.mail import mail_admins
 from django.contrib import messages
 
 from .forms import FeedbackForm
+from .models import Feedback
 
 
 class FeedbackView(CreateView):
-    template_name = 'feedbacks/feedback.html'
+    model = Feedback
     form_class = FeedbackForm
     success_url = reverse_lazy('feedback')
+    exclude = ['create_date']
+    template_name = 'feedbacks/feedback.html'  # template_name = 'feedback.html'
 
     def form_valid(self, form):
         data = form.cleaned_data
@@ -17,3 +20,9 @@ class FeedbackView(CreateView):
         mail_admins(data['subject'], text, fail_silently=True)
         messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!о')
         return super(FeedbackView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+	    context = super(FeedbackView, self).get_context_data(**kwargs)
+	    context['title'] = "Leave a feedback"
+	    return context
+
