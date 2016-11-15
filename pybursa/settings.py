5895fa17eadf59da4ac0bc4a103da4e2f6be856e
OPersian <olena.persianova@gmail.com>
2016-11-15 21:12:01 +0200

    changed: no form

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -79,10 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'TEST': {
-        'CHARSET': 'utf8',
-        # 'COLLATION': 'utf8_general_ci',
     }
 }
 
