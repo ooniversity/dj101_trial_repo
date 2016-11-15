7922303e42760baa82a8bcab5b6457846b0134a1
OPersian <olena.persianova@gmail.com>
2016-11-15 06:26:58 +0200

    added solution

--- courses/views.py
+++ courses/views.py
@@ -1,49 +1,51 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import messages
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
-from .forms import CourseModelForm, LessonModelForm
+from .forms import LessonModelForm
 from .models import Course
 
 
-def detail(request, pk):
-    course = get_object_or_404(Course, pk=pk)
-    return render(request, 'courses/detail.html', {'course': course})
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/detail.html'
 
 
-def create(request):
-    if request.method == "POST":
-        form = CourseModelForm(request.POST)
-        if form.is_valid():
-            course = form.save()
-            messages.success(request, u'Курс {} успешно создан.'.format(course.name))
-            return redirect('index')
-    else:
-        form = CourseModelForm()
-    return render(request, 'courses/add.html', {'form': form})
+class CourseCreateView(CreateView):
+    model = Course
+    template_name = 'courses/add.html'
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        super_valid = super(CourseCreateView, self).form_valid(form)
+        messages.success(self.request,
+                         'Course {} has been successfully added.'.format(self.object.name))
+        return super_valid
 
 
-def edit(request, pk):
-    course = get_object_or_404(Course, pk=pk)
-    if request.method == "POST":
-        form = CourseModelForm(request.POST, instance=course)
-        if form.is_valid():
-            course = form.save()
-            messages.success(request, u'Данные изменены.')
-            return redirect('courses:edit', course.id)
-    else:
-        form = CourseModelForm(instance=course)
-    return render(request, 'courses/edit.html', {'form': form})
+class CourseUpdateView(UpdateView):
+    model = Course
+    template_name = 'courses/edit.html'
+    success_url = '/courses/edit/%(id)d/'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The changes have been saved.')
+        return super(CourseUpdateView, self).form_valid(form)
 
 
-def remove(request, pk):
-    course = get_object_or_404(Course, pk=pk)
-    if request.method == "POST":
-        course.delete()
-        messages.success(request, u'Курс {} был удален.'.format(course.name))
-        return redirect('index')
-    return render(request, 'courses/remove.html', {'course': course})
+class CourseDeleteView(DeleteView):
+    model = Course
+    template_name = 'courses/remove.html'
+    success_url = reverse_lazy('index')
+
+    def delete(self, request, *args, **kwargs):
+        delete_super = super(CourseDeleteView, self).delete(request, *args, **kwargs)
+        messages.success(self.request,
+                         'Course {} has been deleted.'.format(self.object.name))
+        return delete_super
 
 
 def add_lesson(request, pk):
@@ -52,9 +54,8 @@ def add_lesson(request, pk):
         form = LessonModelForm(request.POST)
         if form.is_valid():
             lesson = form.save()
-            messages.success(request, u'Занятие {} было создано.'.format(lesson.subject))
+            messages.success(request, 'Lesson {} has been successfully added.'.format(lesson.subject))
             return redirect('courses:detail', lesson.course_id)
     else:
         form = LessonModelForm(initial={'course': course})
     return render(request, 'courses/add_lesson.html', {'form': form})
-
