6f3a96ad7d9758436fb64f51edf678630315ef00
OPersian <olena.persianova@gmail.com>
2016-11-15 04:32:11 +0200

    added solution

--- students/urls.py
+++ students/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.create, name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
 ]
