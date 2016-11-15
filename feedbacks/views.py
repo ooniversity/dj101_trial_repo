f0b372e15c3ae12dede19a93dfc828ca76e7aa28
OPersian <olena.persianova@gmail.com>
2016-11-15 19:19:26 +0200

    fix

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -8,7 +8,7 @@ from .models import Feedback
 
 
 class FeedbackView(CreateView):
-    model = Feedback
+    model = Feedback  # to avoid ImproperlyConfigured error
     form_class = FeedbackForm
     success_url = reverse_lazy('feedback')
     exclude = ['create_date']
@@ -18,11 +18,10 @@ class FeedbackView(CreateView):
         data = form.cleaned_data
         text = 'Message from {0},\n {1}'.format(data['name'], data['message'])
         mail_admins(data['subject'], text, fail_silently=True)
-        messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!о')
+        messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!')
         return super(FeedbackView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
-	    context = super(FeedbackView, self).get_context_data(**kwargs)
-	    context['title'] = "Leave a feedback"
-	    return context
-
+        context = super(FeedbackView, self).get_context_data(**kwargs)
+        context['title'] = "Leave a feedback"
+        return context
